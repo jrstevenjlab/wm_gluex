ae821318f1b7e5b3a69b2b5d52e0b971f45f3616
Justin R. Stevens <jrstevens01@vortex.sciclone.wm.edu>
2017-05-03 23:18:57 -0400

    add p2kpi0

---
+++ analysis/p2kpi0/runSelector.C
@@ -0,0 +1,57 @@
+// macro to process analysis TTree with TSelector
+#include <iostream> 
+
+#include "TFile.h"
+#include "TTree.h"
+#include "TString.h"
+#include "TSystem.h"
+   
+void runSelector(TString runNumber = "11366", TString myPath = "/sciclone/data10/jrstevens01/RunPeriod-2016-02/analysis/ver05/tree_pi0kmkp/") 
+{
+  // Load DSelector library
+  gROOT->ProcessLine(".x $(ROOT_ANALYSIS_HOME)/scripts/Load_DSelector.C");
+  int Proof_Nthreads = 8;
+
+  // process signal 
+  TString sampleDir = myPath;
+  sampleDir += Form("0%s/", runNumber.Data());
+  cout<<"running selector on files in: "<<sampleDir.Data()<<endl;
+  
+  TChain *chain = new TChain("pi0kmkp_Tree");
+  TSystemDirectory dir(sampleDir, sampleDir);
+  TList *files = dir.GetListOfFiles();
+  int ifile = 0;
+  if(files) {
+	  TSystemFile *file;
+	  TString fileName;
+	  TIter next(files);
+	  
+	  // loop over files
+	  while ((file=(TSystemFile*)next())) {
+		  fileName = file->GetName();
+		  if(fileName.Contains(runNumber)) {
+			  cout<<fileName.Data()<<endl;
+			  
+			  // check if file corrupted
+			  TFile f(sampleDir+fileName);
+			  if(f.TestBit(TFile::kRecovered)) {
+				  cout<<"file corrupted -> skipping"<<endl;
+				  continue;
+			  }
+			  if(f.IsZombie()) {
+				  cout<<"file is a Zombie -> skipping"<<endl;
+				  continue;
+			  }
+			  
+			  // add file to chain
+			  chain->Add(sampleDir+fileName);
+			  ifile++;
+		  }
+	  }
+
+	  cout<<"total entries in TChain = "<<chain->GetEntries()<<" from "<<ifile<<" files"<<endl;
+	  DPROOFLiteManager::Process_Chain(chain, "DSelector_p2kpi0.C+", Form("hist_p2kpi0_%s.acc.root", runNumber.Data()), Proof_Nthreads);
+  }
+
+  return;
+}

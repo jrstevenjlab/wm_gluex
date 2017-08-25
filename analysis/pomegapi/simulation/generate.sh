6e7ce41e95c4ee4079cd409dc69031b23f3201be
jrstevenjlab <jrsteven@jlab.org>
2017-08-25 14:12:57 -0400

    extra environment variables to run on qlogin

--- analysis/pomegapi/simulation/generate.sh
+++ analysis/pomegapi/simulation/generate.sh
@@ -1,4 +1,9 @@
+setenv JANA_RESOURCE_DIR /sciclone/home10/jrstevens01/resources
 setenv JANA_CALIB_CONTEXT "variation=mc"
+setenv JANA_CALIB_URL sqlite:////sciclone/home10/jrstevens01/resources/ccdb.sqlite
+
+setenv CCDB_CONNECTION sqlite:////sciclone/home10/jrstevens01/resources/ccdb.sqlite
+setenv RCDB_CONNECTION sqlite:////sciclone/home10/jrstevens01/resources/rcdb.sqlite
 
 genr8 -M100 -r10000 -s10000 -Aomegapi.txt < omegapi.in
 
@@ -6,7 +11,7 @@ genr8_2_hddm -V"0 0 50 80" omegapi.txt
 
 hdgeant
 
-mcsmear hdgeant.hddm
+mcsmear hdgeant.hddm -PTHREAD_TIMEOUT=300
 
 hd_root --config=hd_root_analysis.conf hdgeant_smeared.hddm
 

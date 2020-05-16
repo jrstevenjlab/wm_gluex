de694f338a4167cf1fb731b8a62ff7aaec1e27cb
Amy M. Schertz <amschertz@email.wm.edu>
2020-05-16 16:28:51 -0400

    add omega_misspip selector and fit code, update omega2pi selector

--- analysis/omega_misspip/selector/runSelector.C
+++ analysis/omega_misspip/selector/runSelector.C
@@ -5,16 +5,20 @@
 #include "TTree.h"
 #include "TString.h"
 #include "TSystem.h"
-//GlueX 2017 data located at: /sciclone/gluex10/RunPeriod-2017-01/analysis/ver36/tree_pi0pippippimpim__B4_M7/merged/
-//GlueX 2018-01 data located at: /sciclone/gluex10/RunPeriod-2018-01/analysis/ver08/tree_pi0pippippimpim__B4_M7/merged/
-//GlueX 2018-08 data located at: /sciclone/gluex10/RunPeriod-2018-08/analysis/ver06/tree_pi0pippippimpim__B4_M7/merged/
-//Simulations located at: /sciclone/gluex10/amschertz/pomega2pi_omega3pi/simulations/test##/root/trees/
-//bggen located at /sciclone/gluex10/gluex_simulations/REQUESTED_MC/2017_bggen_p1/tree_pi0pippippimpim__B4_M7/
    
 
-void runSelector(TString runNumber = "41127", TString myPath = "/sciclone/gluex10/RunPeriod-2018-01/analysis/ver08/tree_pi0pippippimpim__B4_M7/merged/") 
+void runSelector(TString runNumber = "30730", TString myPath = "/sciclone/gluex10/gluex_simulations/recon-2017_01-ver03/gen_omega_3pi/jz_omega_3pi/ver23/tree_pi0pimmisspip__B1_T1_U1_M7/merged/") //use for mc
+
+//void runSelector(TString runNumber = "30276", TString myPath = "/sciclone/gluex10/RunPeriod-2017-01/analysis/ver23/tree_pi0pimmisspip__B1_T1_U1_M7/merged/") //use for data
 
 {
+  bool mc;
+  if(myPath == "/sciclone/gluex10/gluex_simulations/recon-2017_01-ver03/gen_omega_3pi/jz_omega_3pi/ver23/tree_pi0pimmisspip__B1_T1_U1_M7/merged/") {
+    mc = true;
+  }
+  else {
+    mc = false;  
+  }
   // Load DSelector library
   gROOT->ProcessLine(".x $(ROOT_ANALYSIS_HOME)/scripts/Load_DSelector.C");
   int Proof_Nthreads = 8;
@@ -24,7 +28,7 @@ void runSelector(TString runNumber = "41127", TString myPath = "/sciclone/gluex1
   //sampleDir += Form("0%s/", runNumber.Data());
   cout<<"running selector on files in: "<<sampleDir.Data()<<endl;
   
-  TChain *chain = new TChain("pi0pippippimpim__B4_M7_Tree");
+  TChain *chain = new TChain("pi0pimmisspip__B1_T1_U1_M7_Tree");
   TSystemDirectory dir(sampleDir, sampleDir);
   TList *files = dir.GetListOfFiles();
   int ifile = 0;
@@ -57,7 +61,12 @@ void runSelector(TString runNumber = "41127", TString myPath = "/sciclone/gluex1
 	  }
 
 	  cout<<"total entries in TChain = "<<chain->GetEntries()<<" from "<<ifile<<" files"<<endl;
-	  DPROOFLiteManager::Process_Chain(chain, "DSelector_pomega2pi_omega3pi.C+", Proof_Nthreads, Form("hist_pomega2pi_omega3pi_%s.acc.root", runNumber.Data()));
+	  if(mc == true){
+	    DPROOFLiteManager::Process_Chain(chain, "DSelector_omega_misspip.C+", Proof_Nthreads, Form("hist_omega_misspip_gen_%s.acc.root", runNumber.Data()));
+	  }
+	  else {
+	    DPROOFLiteManager::Process_Chain(chain, "DSelector_omega_misspip.C+", Proof_Nthreads, Form("hist_omega_misspip_data_%s.acc.root", runNumber.Data()));
+	  }
   }
 
   return;
